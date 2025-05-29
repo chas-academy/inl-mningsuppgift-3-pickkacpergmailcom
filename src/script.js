ae20b146487c1064409db424deed573f53b22ff6
pick.kacper@gmail.com <pick.kacper@gmail.com>
2025-05-29 19:28:44 +0200

    klar

---
+++ src/script.js
@@ -0,0 +1,49 @@
+const productInput = document.getElementById("productInput")
+const priceInput = document.getElementById("priceInput")
+const addButton = document.getElementById("addButton")
+const cartList = document.getElementById("cartList")
+
+let cart = []
+
+function addToCart(){
+    const productName = productInput.value
+    const productPrice = Number(priceInput.value)
+
+    const newProduct = {
+        productName: productName,
+        productPrice: productPrice,
+        quantity: 1,
+    }
+
+    let itemExists = false
+
+    for (item of cart){
+        if (item.productName === productName){
+            item.quantity++
+            itemExists = true
+        }
+    }
+
+    if (!itemExists){
+        cart.push(newProduct)
+    }
+
+    productInput.value = ""
+    priceInput.value = ""
+
+    renderCart()
+}
+
+function renderCart(){
+    cartList.innerHTML = ""
+    
+    for (item of cart){
+        const li = document.createElement("li")
+        li.textContent = `${item.productName}, ${item.productPrice} kr (x${item.quantity})`
+        cartList.appendChild(li)
+    }
+}
+
+addButton.addEventListener("click", () => {
+    addToCart()
+})
